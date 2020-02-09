bd3fc782b4972fbcb3cd94fbe155361c98589ff2
Alee Martinezz <48629217+leemrtnzz@users.noreply.github.com>
2020-02-09 23:57:12 +0700

    Add files via upload

---
+++ Page/Mapel/ASJ/ASJ.php
@@ -0,0 +1,6 @@
+<?php include "page/body.php"?>
+<center>
+<body>
+	<p>----------- Content -----------<br>--- ASJ XII ---<br><a href="#" target="_blank" rel="nofollow">Munin</a><br><a href="page/mapel/ASJ/nagios.txt" target="_blank" rel="nofollow">Nagios</a><br><a href="#" target="_blank" rel="nofollow">Web Proxy - Squid</a><br>--------- End-Content ---------</p>
+</center>
+</body>
